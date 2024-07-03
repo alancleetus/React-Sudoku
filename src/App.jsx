6b01c294774f35915df7c8d2a991160477e92984
Alan Cleetus <alanclee95@gmail.com>
2024-07-02 17:37:07 -0700

    Move validator

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,63 @@ import { useState } from "react";
 import "./App.css";
 import InputButton from "./components/InputButton";
 import InputCell from "./components/InputCell";
+import SudokuGrid from "./components/ExampleGrid";
+
 function App() {
   const [currInputNumber, setCurrInputNumber] = useState(-1);
+  const [solveGrid, setSolveGrid] = useState(SudokuGrid);
 
   const numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  function handleCellValueChange(row, col, newValue) {
+    //console.log({ row, col, newValue });
+    return validateNewCellValue({ row, col, newValue });
+  }
+
+  const validateNewCellValue = ({ row, col, newValue }) => {
+    //check each row for new value
+    const isInRow = solveGrid[row].find((i) => {
+      return i == newValue;
+    })
+      ? true
+      : false;
+
+    //check each col for new value
+    const isInCol = solveGrid
+      .map((eachRow) => {
+        return eachRow[col] == newValue;
+      })
+      .find((flag) => {
+        return flag === true;
+      })
+      ? true
+      : false;
+    isInRow && console.log(`${newValue} is in row`);
+    isInCol && console.log(`${newValue} is in col`);
+
+    /*
+    ! TODO: add code to validate 3x3 cells
+    */
+
+    return true;
+  };
 
   return (
     <>
       <h1>Sudoku</h1>
 
-      {[...Array(9).keys()].map((j) => {
+      {[...Array(9).keys()].map((i) => {
         return (
-          <div className="sudoku-row" key={j}>
-            {[...Array(9).keys()].map((i) => {
+          <div className="sudoku-row" key={i}>
+            {[...Array(9).keys()].map((j) => {
               return (
                 <InputCell
-                  key={i}
-                  row={j}
-                  col={i}
+                  key={"row" + i + "col" + j}
+                  row={i}
+                  col={j}
+                  initialValue={SudokuGrid[i][j]}
                   currInputNumber={currInputNumber}
+                  handleCellValueChange={handleCellValueChange}
                 />
               );
             })}
--- src/components/InputCell.jsx
+++ src/components/InputCell.jsx
@@ -2,16 +2,28 @@ import PropTypes from "prop-types";
 
 import { useState } from "react";
 
-export default function InputCell({ currInputNumber, row, col }) {
-  const [cellValue, setCellValue] = useState(null);
+export default function InputCell({
+  currInputNumber,
+  row,
+  col,
+  initialValue,
+  handleCellValueChange,
+}) {
+  const [cellValue, setCellValue] = useState(
+    initialValue > 0 ? initialValue : null
+  );
 
   function onClickCell() {
-    setCellValue(currInputNumber > 0 ? currInputNumber : null);
+    if (initialValue == 0) {
+      handleCellValueChange(row, col, currInputNumber);
+      setCellValue(currInputNumber > 0 ? currInputNumber : null);
+    }
   }
 
   let classes = "sudoku-cell";
   if (row === 2 || row === 5) classes = "sudoku-cell bottom-margin ";
   if (col === 2 || col === 5) classes = classes + " right-margin ";
+  if (initialValue > 0) classes = classes + " constant-value";
 
   return (
     <div className={classes} onClick={onClickCell}>
@@ -24,4 +36,6 @@ InputCell.propTypes = {
   currInputNumber: PropTypes.number.isRequired,
   row: PropTypes.number.isRequired,
   col: PropTypes.number.isRequired,
+  initialValue: PropTypes.number.isRequired,
+  handleCellValueChange: PropTypes.func.isRequired,
 };
