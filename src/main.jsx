4b3be59386603eb445b0328a9619c2871dc3dad4
Alan Cleetus <alanclee95@gmail.com>
2024-12-01 05:12:54 -0800

    Adding themes to main app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./theme.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function loadTheme() {
+  const savedTheme = localStorage.getItem("theme") || "light"; // Default to light theme
+  document.documentElement.setAttribute("data-theme", savedTheme);
+}
+
+loadTheme();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
